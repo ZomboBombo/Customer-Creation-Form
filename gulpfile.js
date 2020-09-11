f2dcd7034f8c05567382a3464a2d296c597d2502
Sergey Novikov <foxinazombobombo@mail.ru>
2020-09-11 13:39:31 +0300

    Adds service files and folders
    Service files and folders have been added to the project.
    The structure of the project was also organized.

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var gulp = require('gulp');
+
+// --- Helper utilities ---
+var rename = require('gulp-rename');
+var del = require('del');
+
+// --- Preprocessor utilities ---
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+
+
+// --- JS-utilities ---
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var pipeline = require('readable-stream').pipeline;
+
+// --- Server utitlities ---
+var server = require('browser-sync').create();
+
+
+/*
+___________________________________________________________________________
+
+---------------------------------- TASKS ----------------------------------
+___________________________________________________________________________
+
+*/
+
+// *** JS-files handling ***
+gulp.task('scripts', () => {
+	return pipeline(
+		gulp.src('source/js_modules/**/*.js'),
+    uglify(),
+    concat('scripts.min.js'),
+    gulp.dest('source')
+	);
+});
+
+
+// *** Raising the server ***
+gulp.task('server', () => {
+  server.init({
+    server: '',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch('source/js_modules/**/*.js', gulp.series('scripts', 'refresh'));
+  gulp.watch('*.html', gulp.series('refresh'));
+});
+
+gulp.task('refresh', done => {
+  server.reload();
+  done();
+});
+
+
+// *** Main tasks ***
+gulp.task('start', gulp.series('server'));
---
+++ source/js_modules/moduleSecond.js
@@ -0,0 +1,7 @@
+var moduleSecond = [];
+
+for (var i = 0; i < 6; i++) {
+	moduleSecond.push(i);
+}
+
+console.log('This is a second module:', moduleSecond);
