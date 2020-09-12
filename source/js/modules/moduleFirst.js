d9adc5f20e500fb26abac315fac30e17ac4e62b4
Sergey Novikov <foxinazombobombo@mail.ru>
2020-09-12 23:02:33 +0300

    Removes unused JS-modules

--- source/js/modules/moduleFirst.js
+++
@@ -1,7 +0,0 @@
-var moduleFirst = [];
-
-for (var i = 0; i < 5; i++) {
-	moduleFirst.push(i);
-}
-
-console.log('This is a first module:', moduleFirst);
--- source/js/modules/moduleSecond.js
+++
@@ -1,7 +0,0 @@
-var moduleSecond = [];
-
-for (var i = 0; i < 6; i++) {
-	moduleSecond.push(i);
-}
-
-console.log('This is a second module:', moduleSecond);
