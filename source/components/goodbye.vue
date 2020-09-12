bb5e1ab3739d2e9452aa9738af957a4636de3e3d
Sergey Novikov <foxinazombobombo@mail.ru>
2020-09-12 23:08:49 +0300

    Adds Vue-necessary folders and files
    In project have been added:
    
    	* main file of Vue-application — "app.js";
    
    	* folder for Vue-components — "source/components";
    
    	* compiled Vue-application file — "bundle.js".

---
+++ source/components/goodbye.vue
@@ -0,0 +1,23 @@
+<template>
+	<p class="vue-goodbye">{{ vueGoodbye }}</p>
+</template>
+
+<script>
+	module.exports = {
+		data: function () {
+			return {
+				vueGoodbye: "Bye! I'm Vue :)"
+			}
+		}
+	}
+</script>
+
+<style>
+	.vue-goodbye {
+		font-family: 'Roboto', sans-serif;
+		font-size: 25px;
+		font-weight: 700;
+		line-height: 30px;
+		color: red;
+	}
+</style>
---
+++ source/components/hello.vue
@@ -0,0 +1,23 @@
+<template>
+	<h2 class="vue-greeting">{{ vueGreeting }}</h2>
+</template>
+
+<script>
+	module.exports = {
+		data: function () {
+			return {
+				vueGreeting: "Hi! I'm Vue :)"
+			}
+		}
+	}
+</script>
+
+<style>
+	.vue-greeting {
+		font-family: 'Roboto', sans-serif;
+		font-size: 25px;
+		font-weight: 700;
+		line-height: 30px;
+		color: royalblue;
+	}
+</style>
