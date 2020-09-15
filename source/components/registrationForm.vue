2d23fd672d803c0106c7ca966ca2e1b4839c00d0
Sergey Novikov <foxinazombobombo@mail.ru>
2020-09-15 16:43:40 +0300

    Adds class attributes to the elements
    To the markup have been added class attributes based on BEM-methology.

--- source/components/registrationForm.vue
+++ source/components/registrationForm.vue
@@ -1,11 +1,11 @@
 <template>
   <form class="registration-form" @submit.prevent="onSubmit">
-    <fieldset>
-      <legend>Личные данные</legend>
+    <fieldset class="registration-form__data-field data-fieldset">
+      <legend class="data-fieldset__heading">Личные данные</legend>
 
-      <ul>
-        <li>
-          <label for="surname">Фамилия:</label>
+      <ul class="data-fieldset__data-list list-of-data">
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="surname">Фамилия:</label>
           <input type="text" name="client-surname"
                 id="surname"
                 :class="{invalid: ($v.surname.$dirty && !$v.surname.required)}"
@@ -14,8 +14,8 @@
 
           <span v-if="$v.surname.$dirty && !$v.surname.required">Необходимо ввести Фамилию!</span>
         </li>
-        <li>
-          <label for="name">Имя:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="name">Имя:</label>
           <input type="text" name="client-name"
                 id="name"
                 :class="{invalid: ($v.name.$dirty && !$v.name.required)}"
@@ -24,102 +24,115 @@
 
           <span v-if="$v.name.$dirty && !$v.name.required">Необходимо ввести Имя!</span>
         </li>
-        <li>
-          <label for="patronymic">Отчество:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="patronymic">Отчество:</label>
           <input type="text" name="client-patronymic" id="patronymic" placeholder="Иванович">
         </li>
-        <li>
-          <label for="birthday">Дата рождения:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="birthday">Дата рождения:</label>
           <input type="date" name="client-birthday"
                 id="birthday"
                 :class="{invalid: ($v.birthday.$dirty && !$v.birthday.required)}"
                 v-model.trim="$v.birthday.$model">
+
+          <span v-if="$v.birthday.$dirty && !$v.birthday.required">Укажите, пожалуйста, дату рождения.</span>
         </li>
-        <li>
-          <label for="phone">Номер телефона:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="phone">Номер телефона:</label>
           <input type="tel" name="client-phone"
                 id="phone"
-                :class="{invalid: ($v.phone.$dirty && !$v.phone.required)}"
+                :class="{invalid: ($v.phone.$dirty && !$v.phone.required || $v.phone.$dirty && !$v.phone.validFormat)}"
                 v-model.trim="$v.phone.$model"
-                placeholder="+7 ХХХ ХХХ ХХ ХХ">
+                placeholder="7 ХХХ ХХХ ХХ ХХ"
+                maxlength="11">
+
+          <span v-if="$v.phone.$dirty && !$v.phone.validFormat">Введённое значение не соответсвует паттерну ввода!</span>
         </li>
-        <li>
+        <li class="list-of-data__item">
           <div>
             <span>Пол:</span>
 
-            <label for="female">Ж</label>
+            <label class="list-of-data__label" for="female">Ж</label>
             <input type="radio" name="client-sex" id="female" checked>
 
-            <label for="male">М</label>
+            <label class="list-of-data__label" for="male">М</label>
             <input type="radio" name="client-sex" id="male">
           </div>
         </li>
-        <li>
-          <label for="category">Группа клиентов:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="category">Группа клиентов:</label>
           <select name="client-category"
                   id="category"
-                  :class="{invalid: ($v.category.$dirty && !$v.category.required)}"
-                  v-model="$v.category.$model">
-            <option>VIP</option>
-            <option>Проблемные</option>
-            <option>ОМС</option>
+                  :class="{invalid: ($v.сategories.$dirty && !$v.сategories.required)}"
+                  v-model="$v.сategories.$model" multiple>
+
+            <option v-for="option in options" :value="option">{{ option }}</option>
+          </select>
+
+          <span v-if="$v.сategories.$dirty && !$v.сategories.required">Пожалуйста, выберите категорию.</span>
+
+          <ol>
+            <li v-for="option in сategories">{{ option }}</li>
+          </ol>
+        </li>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="doctor">Лечащий врач:</label>
+          <select name="attending-doctor"
+                  id="doctor"
+                  v-model="$v.availableDoctors.$model">
+
+            <option v-for="doctor in availableDoctors" :value="doctor">{{ doctor }}</option>
           </select>
         </li>
-        <li>
-          <label for="doctor">Лечащий врач:</label>
-          <select name="attending-doctor" id="doctor">
-            <option>Иванов</option>
-            <option>Захаров</option>
-            <option>Чернышева</option>
-          </select>
-        </li>
-        <li>
-          <label for="message">Не отправлять СМС</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="message">Не отправлять СМС</label>
           <input type="checkbox" name="client-message" id="message">
         </li>
       </ul>
     </fieldset>
     
-    <fieldset>
-      <legend>Адрес</legend>
+    <fieldset class="registration-form__data-field data-fieldset">
+      <legend class="data-fieldset__heading">Адрес</legend>
 
-      <ul>
-        <li>
-          <label for="postcode">Индекс:</label>
+      <ul class="data-fieldset__data-list list-of-data">
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="postcode">Индекс:</label>
           <input type="number" name="client-postcode" id="postcode">
         </li>
-        <li>
-          <label for="country">Страна:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="country">Страна:</label>
           <input type="text" name="client-country" id="country">
         </li>
-        <li>
-          <label for="region">Область:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="region">Область:</label>
           <input type="text" name="client-region" id="region">
         </li>
-        <li>
-          <label for="city">Город:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="city">Город:</label>
           <input type="text" name="client-city"
                 id="city"
                 :class="{invalid: ($v.city.$dirty && !$v.city.required)}"
                 v-model.trim="$v.city.$model">
+
+          <span v-if="$v.city.$dirty && !$v.city.required">Укажите, пожалуйста, город, в котором проживаете.</span>
         </li>
-        <li>
-          <label for="street">Улица:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="street">Улица:</label>
           <input type="text" name="client-street" id="street">
         </li>
-        <li>
-          <label for="housenumber">Дом:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="housenumber">Дом:</label>
           <input type="number" name="client-housenumber" id="housenumber">
         </li>
       </ul>
     </fieldset>
 
-    <fieldset>
-      <legend>Паспорт</legend>
+    <fieldset class="registration-form__data-field data-fieldset">
+      <legend class="data-fieldset__heading">Паспорт</legend>
 
-      <ul>
-        <li>
-          <label for="document">Тип документа:</label>
+      <ul class="data-fieldset__data-list list-of-data">
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="document">Тип документа:</label>
           <select name="client-document"
                   id="document"
                   :class="{invalid: ($v.document.$dirty && !$v.document.required)}"
@@ -128,30 +141,39 @@
             <option>Свидетельство о рождении</option>
             <option>Водительское удостоверение</option>
           </select>
+
+          <span v-if="$v.document.$dirty && !$v.document.required">Выберите тип документа для регистрации.</span>
         </li>
-        <li>
-          <label for="passportSeries">Серия:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="passportSeries">Серия:</label>
           <input type="number" name="client-passport" id="passportSeries">
         </li>
-        <li>
-          <label for="passportNumber">Номер:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="passportNumber">Номер:</label>
           <input type="number" name="client-passport" id="passportNumber">
         </li>
-        <li>
-          <label for="placeOfIssue">Кем выдан:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="placeOfIssue">Кем выдан:</label>
           <input type="text" name="client-passport" id="placeOfIssue">  
         </li>
-        <li>
-          <label for="dateOfIssue">Дата выдачи:</label>
+        <li class="list-of-data__item">
+          <label class="list-of-data__label" for="dateOfIssue">Дата выдачи:</label>
           <input type="date" name="client-passport"
                 id="dateOfIssue"
                 :class="{invalid: ($v.dateOfIssue.$dirty && !$v.dateOfIssue.required)}"
                 v-model="$v.dateOfIssue.$model">
+
+          <span v-if="$v.dateOfIssue.$dirty && !$v.dateOfIssue.required">Необходимо указать дату выдачи документа.</span>
         </li>
       </ul>
     </fieldset>
 
     <button type="submit">Зарегистрироваться</button>
+
+    <div class="modal-form">
+      <p class="">ФОРМА ПРОШЛА ВАЛИДАЦИЮ!</p>
+      <p class="">Клиент успешно зарегистрирован!</p>
+    </div>
   </form>
 </template>
 
@@ -166,7 +188,17 @@
         name: '',
         birthday: null,
         phone: null,
-        category: null,
+        сategories: [],
+        options: [
+          'VIP',
+          'Проблемные',
+          'ОМС'
+        ],
+        availableDoctors: [
+          'Иванов',
+          'Захаров',
+          'Чернышева'
+        ],
         city: '',
         document: null,
         dateOfIssue: null
@@ -184,11 +216,12 @@
       },
       phone: { 
         required,
-        maxLength: maxLength(11)
+        validFormat: enteredVal => /^7[\d]+$/.test(enteredVal)
       },
-      category: { 
+      сategories: { 
         required
       },
+      availableDoctors: {},
       city: { 
         required
       },
@@ -218,16 +251,12 @@
 
           return;
         } else {
+          var modalForm = document.querySelector('.modal-form');
+          modalForm.classList.add('modal-form--valid');
+
           form.submit();
         }
       }
     }
   }
 </script>
-
-
-<style>
-  .invalid {
-    outline: 2px solid red;
-  }
-</style>
